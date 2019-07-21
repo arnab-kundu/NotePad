c771e470ab0d2e4d38eb354ffb9d02e31749c4e0
ARNAB <arnab.kundu1990@gmail.com>
2019-07-21 06:09:27 +0530

    Created ObservableEditText as view helper Prepared layout file for TwoWay DataBinding

---
+++ app/src/main/java/com/arnab/notepad/views/viewhelpers/ObservableEditText.java
@@ -0,0 +1,18 @@
+package com.arnab.notepad.views.viewhelpers;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class ObservableEditText extends BaseObservable {
+
+    private String text;
+
+    @Bindable
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
